d161c4af80b6ac045f733364f25121c8f9715669
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2021-11-07 20:45:36 -0600

    First draft of react typescript template

---
+++ react/typescript/README.md
@@ -0,0 +1,4 @@
+# React Typescript Template
+
+This template includes the following technologies all working together happily.
+
---
+++ react/typescript/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// Styles
+import "./App.scss";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <main>Main Content Goes here</main>
+    </div>
+  );
+};
+
+export default App;
